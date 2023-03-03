b58bb62595d1b9d8cb64ec6171c871bf3a086b29
Nathan Dariva <nathan.gd26@hotmail.com>
2023-03-02 22:32:37 -0300

    Adicionado a funcionalidade de alteração de senha

--- cypress/support/enums/index.ts
+++ cypress/support/enums/index.ts
@@ -2,10 +2,12 @@ import REGISTER from "./register";
 import HOME from "./home";
 import LOGIN from "./login"
 import NEW_ARTICLE from "./new-article";
+import SETTINGS from './settings'
 
 export {
     REGISTER,
     HOME,
     LOGIN,
-    NEW_ARTICLE
+    NEW_ARTICLE,
+    SETTINGS
 }
