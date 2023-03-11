79ac126ec48427eecbc8bdb7d62f199f1ecf9f17
Nathan Dariva <nathan.gd26@hotmail.com>
2023-03-11 00:17:25 -0300

    validando mensagens de erro na pagina artigos

--- cypress/support/enums/article.ts
+++ cypress/support/enums/article.ts
@@ -1,6 +1,7 @@
 enum ARTICLE {
     DIV_TITLE = 'div h1',
-    BNT_DELETE = "button[class*='btn-outline-dange']"
+    BNT_DELETE = "button[class*='btn-outline-dange']",
+    MSG_ERROR = 'list-errors li'
   }
   
   export default ARTICLE
